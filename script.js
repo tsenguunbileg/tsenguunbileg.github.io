7a144ae8204c609fd9aa7a399043a1f129356ca1
tsenguunbileg <ch.tsengun@gmail.com>
2025-01-17 12:34:28 +0800

    summary

---
+++ script.js
@@ -0,0 +1,92 @@
+const input = document.getElementById("search-input");
+const button = document.getElementById("search-button");
+const pokemonID = document.getElementById("pokemon-id");
+const pokemonName = document.getElementById("pokemon-name");
+const spriteContainer = document.getElementById("sprite-container");
+const types = document.getElementById("types");
+const pokemonHeight = document.getElementById("height");
+const pokemonWeight = document.getElementById("weight");
+const hp = document.getElementById("hp");
+const attack = document.getElementById("attack");
+const defense = document.getElementById("defense");
+const specialAttack = document.getElementById("special-attack");
+const specialDefense = document.getElementById("special-defense");
+const speed = document.getElementById("speed");
+
+const output = document.getElementById("top-container");
+
+const apiURL = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon";
+
+const getPokemon = async () => {
+  try {
+    const pokemonNameOrId = input.value.toLowerCase();
+    const response = await fetch(`${apiURL}/${pokemonNameOrId}`);
+    const data = await response.json();
+    const {
+      name,
+      id,
+      weight,
+      height,
+      sprites: { front_default: frontSprite },
+      stats: [
+        { base_stat: hpStat },
+        { base_stat: attackStat },
+        { base_stat: defenseStat },
+        { base_stat: specialAttackStat },
+        { base_stat: specialDefenseStat },
+        { base_stat: speedStat },
+      ],
+    } = data;
+    const typeNames = data.types.map(({ type: { name } }) => name);
+
+    pokemonName.textContent = name.toUpperCase();
+    pokemonID.textContent = "#" + id;
+    pokemonWeight.textContent = "Weight: " + weight;
+    pokemonHeight.textContent = "Height: " + height;
+    hp.textContent = hpStat;
+    attack.textContent = attackStat;
+    defense.textContent = defenseStat;
+    specialAttack.textContent = specialAttackStat;
+    specialDefense.textContent = specialDefenseStat;
+    speed.textContent = speedStat;
+
+    spriteContainer.innerHTML = `<img src="${frontSprite}" id="sprite" class="size-1/3">`;
+    types.innerHTML = typeNames
+      .map((type) => {
+        return `<span class="type ${type} bg-${type==='electric' ? 'cyan' : 'slate'}-200 p-1">${type}</span>`;
+      })
+      .join("");
+  } catch (err) {
+    console.log(err);
+    //alert("Pokémon not found");
+    pokemonName.textContent = "Pokémon not found";
+  }
+};
+
+const clearUI = () => {
+  pokemonName.textContent = "";
+  pokemonID.textContent = "";
+  pokemonWeight.textContent = "";
+  pokemonHeight.textContent = "";
+  hp.textContent = "";
+  attack.textContent = "";
+  defense.textContent = "";
+  specialAttack.textContent = "";
+  specialDefense.textContent = "";
+  speed.textContent = "";
+
+  spriteContainer.innerHTML = "";
+  types.innerHTML = "";
+};
+
+button.addEventListener("click", (event) => {
+  event.preventDefault();
+  clearUI();
+  getPokemon();
+});
+
+input.addEventListener("input", (event) => {
+    clearUI();
+    getPokemon();
+  });
+
